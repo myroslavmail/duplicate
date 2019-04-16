8ad1e566c0c2d485127b999f1a94ce4f52cf730a
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-16 17:52:00 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,7 +2,7 @@
 
 volume_backup () {
     aws_vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values='$tag_name' Name=tag:Usage,Values='$tag_usage' --query "Volumes[].{ID:VolumeId}" --output=text)
-    echo this is my optarg $OPTARG "$OPTARG"
+    echo this is my $aws_vol_id
     #aws ec2 describe-volume-status --profile backup --volume-ids $aws_vol_id
     #aws ec2 create-snapshot --volume-id @aws_vol_id --tag-specifications 'ResourceType=snapshot,Tags=[*]'
     if [ $? -eq 0 ]; then
