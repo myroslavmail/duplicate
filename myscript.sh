6d04121dea697a3ac935396c19cf195d87b67f26
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-16 17:57:52 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 volume_backup () {
-    aws_vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values='$tag_name' Name=tag:Usage,Values='$tag_usage' --query "Volumes[].{ID:VolumeId}" --output=text)
+    aws_vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
     echo this is my $aws_vol_id
     #aws ec2 describe-volume-status --profile backup --volume-ids $aws_vol_id
     #aws ec2 create-snapshot --volume-id @aws_vol_id --tag-specifications 'ResourceType=snapshot,Tags=[*]'
