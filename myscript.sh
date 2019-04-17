bbef13650fb03958387bc9c2deecf17983422089
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-17 18:20:07 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 volume_backup () {
-    aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text|while read line; do aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="*",Value="*"}]'; done
+    vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
+    echo vol_id is $vol_id
+    vol_tags=${aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{Tag:Tags}" --output=text)
+    echo vol_tags is $vol_tags
+    # |while read line; do aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="*",Value="*"}]'; done
     if [ $? -eq 0 ]; then
         echo snapshot is taken
     else
