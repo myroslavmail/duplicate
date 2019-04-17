4328e18f1407beb80448b97ad01aa795ec126fa1
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-17 19:25:12 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 volume_backup () {
-    # vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
-    # aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text|while read line; do aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="*",Value="*"}]'; done
-    aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{Tag:Tags}" --output=text|while read -r a b c; do aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text|while read line; do aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="$a",Value="$b"}]'; done; done
+#   vol_id=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
+    $vol_ids=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
+    echo $vol_ids|while read line; do
+        aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="*",Value="*"}]';
+    done
+    
+    # aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{Tag:Tags}" --output=text|while read -r a b c; do echo $a $b $c; done
 #   $vol_tags | while read -r a b c; do echo $b $c; done
 #   $vol_id | while read line; do aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=[{Key="*",Value="*"}]'; done
     if [ $? -eq 0 ]; then
