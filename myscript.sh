a792b92a3e99745c30464670a7d989b221eaf4e6
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-18 18:52:37 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,11 +2,11 @@
 
 volume_backup () {
     vol_ids=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
-    
+    my_date=$(date +%H_%M_%S_%u_%d_%m)
     echo $vol_ids|tr ' +' '\n'
     echo $vol_ids|tr ' +' '\n'|while read line; do
         tags_list=$(aws ec2 describe-volumes --profile backup --volume-ids $line --output=json|jq .Volumes[].Tags[]|tr -d ' +\n"'|sed -r 's/\}\{/\}\,\{/g'|tr ':' '=');
-        aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=['$tags_list']';
+        aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=['$tags_list',{Key=Date,Value='$my_date'}]';
     done
 
     if [ $? -eq 0 ]; then
