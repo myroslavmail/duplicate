462050bf60273188d57709f59940b6632454de0e
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-18 11:35:58 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,7 +2,7 @@
 
 volume_backup () {
     vol_ids=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
-    tags_list=$(aws ec2 describe-volumes --profile backup --volume-ids $vol_ids --output=json|jq .Volumes[].Tags[]|tr -d ' +\n"'|sed -r 's/\}\{/\}\,\{/g'|tr ':' '=')
+    tags_list=$(aws ec2 describe-volumes --profile backup --volume-ids echo $vol_ids --output=json|jq .Volumes[].Tags[]|tr -d ' +\n"'|sed -r 's/\}\{/\}\,\{/g'|tr ':' '=')
     
     echo $vol_ids|while read line; do
         aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications ResourceType=snapshot,Tags=[echo $tags_list];
