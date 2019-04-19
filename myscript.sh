705ee190633def515bbe30240e5a14d174bad5a9
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-19 16:07:52 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 volume_backup () {
-    vol_ids=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
-    echo $vol_ids|tr ' +' '\n'
-    echo $vol_ids|tr ' +' '\n'|while read line; do
-        tags_list=$(aws ec2 describe-volumes --profile backup --volume-ids $line --output=json|jq .Volumes[].Tags[]|tr -d ' +\n"'|sed -r 's/\}\{/\}\,\{/g'|tr ':' '=');
-        aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=['$tags_list',{Key=Day_Month,Value='$(date +%d_%m)'},{Key=Day_Week,Value='$(date +%u)'}]';
-    done
-
-    if [ $? -eq 0 ]; then
+#    vol_ids=$(aws ec2 describe-volumes --profile backup --filters Name=tag:Name,Values=$tag_name Name=tag:Usage,Values=$tag_usage --query "Volumes[].{ID:VolumeId}" --output=text)
+#    echo $vol_ids|tr ' +' '\n'
+#    echo $vol_ids|tr ' +' '\n'|while read line; do
+#        tags_list=$(aws ec2 describe-volumes --profile backup --volume-ids $line --output=json|jq .Volumes[].Tags[]|tr -d ' +\n"'|sed -r 's/\}\{/\}\,\{/g'|tr ':' '=');
+#        aws ec2 create-snapshot --profile backup --volume-id $line --tag-specifications 'ResourceType=snapshot,Tags=['$tags_list',{Key=Day_Month,Value='$(date +%d_%m)'},{Key=Day_Week,Value='$(date +%u)'}]';
+#    done
+#
+#    if [ $? -eq 0 ]; then
         echo snapshot is taken
-    else
-        echo FAIL 1
-        exit 1
-    fi
+#    else
+#        echo FAIL 1
+#        exit 1
+#    fi
 }
 
 data_maintenance () {
     echo "Remove backed up snapshot(s) when '$OPTARG' days old, except those that not !created on Sat and/or 31||30||29||28 day of the month"
-    rem_date=$((`date +%F -d "-'$OPTARG'"))
+    rem_date=$(date +%F -d "-'$OPTARG' days")
     echo $rem_date
     aws ec2 describe-snapshots --profile backup --filters Name=volume-id,Values=vol-00161d785e1ce2446 --output=json --query 'Snapshots[?StartTime<=$rem_date]'
     
