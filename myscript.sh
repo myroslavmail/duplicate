c68ec1a6a9ac185c726fd0bc9cd8cfc5f407014c
myroslavmail <48326092+myroslavmail@users.noreply.github.com>
2019-04-26 11:28:25 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -116,6 +116,6 @@ echo !!!! SHOW ALL SNAPSHOTS TO BE REMOVED !!!
 data_maintenance
 echo !!!! NOW REMOVE THEM ALL !!!
 data_maintenance|while read line; do
-   delete-snapshot --profile backup --snapshot-id $line;
+   aws ec2 delete-snapshot --profile backup --snapshot-id $line;
    echo !!!! REMOVAL SUCCESSFULLY COMPLETED !!!
 done
